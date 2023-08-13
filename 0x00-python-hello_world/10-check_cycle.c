debbb06c67d30ddca330bc5ae492012a68b0d859
LauraFej <laurafejuo@gmail.com>
2023-08-13 02:12:00 -0700

    checks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,17 +5,16 @@
  * @list: The singly linked list to check
  * Return: 1 if has a cycle in it or 0 if not
  */
-int check_cycle(listinit_t *list)
+int check_cycle(listint_t *list)
 {
 	listint_t *turtle = list, *hare = list;
 	int found = 0;
 
-	while ((turtle && hare_ && hare->next)
+	while ((turtle && hare) && hare->next)
 	{
 		turtle = turtle->next;
 		if (hare->next || hare->next->next)
 			hare = hare->next->next;
-
 		else
 			break;
 		if (turtle == hare)
